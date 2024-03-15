3a1cc227d19cf55965b03e77898354910f4868ae
TheSpitefulOctopus <TheSpitefulOctopus@users.noreply.github.com>
2024-03-15 09:21:53 -0600

    Hello world -- the first program in rust
    001. This is the first program written in rust

---
+++ Hello World/hello.rs
@@ -0,0 +1,8 @@
+// This is a comment and is ignorted by the compiler
+// this is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+    
+    // print text to the console
+    println!("Hello World!");
+}
